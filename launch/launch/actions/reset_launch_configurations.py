3ea23c617856583dbb3fd13f3b436db0ba7ed62d
Kenji Brameld (TRACLabs) <122934495+kbrameld@users.noreply.github.com>
2025-06-02 10:55:36 -0500

    Using ``TimerAction`` with ``SetParameter`` from launch_ros causes crash (#879)
    Signed-off-by: Kenji Brameld <kbrameld@traclabs.com>

--- launch/launch/actions/reset_launch_configurations.py
+++ launch/launch/actions/reset_launch_configurations.py
@@ -20,6 +20,8 @@ from typing import Any
 from typing import Dict
 from typing import List
 from typing import Optional
+from typing import Tuple
+from typing import Type
 
 from ..action import Action
 from ..frontend import Entity
